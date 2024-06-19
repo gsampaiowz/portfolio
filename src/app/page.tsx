4b3b86232175fb3ebec5032943f0159953fe52e4
gsampaiowz <gabrielsampaio1216@gmail.com>
2024-06-19 12:15:46 -0300

    Portfolio iniciado

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="border-2 w-[600px] h-[600px] absolute top-[calc(50%-300px)] left-[calc(50%-300px)] rounded-full blur-lg"></div>
+    </main>
+  );
+}
